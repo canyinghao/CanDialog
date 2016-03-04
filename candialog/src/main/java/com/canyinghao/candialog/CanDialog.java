e146a3ecdce7c50c7e9d404237bc3d8db8f55c5b
canyinghao <canyinghao@hotmail.com>
2016-03-04 09:52:34 +0800

    add jcenter

--- candialog/src/main/java/com/canyinghao/candialog/CanDialog.java
+++ candialog/src/main/java/com/canyinghao/candialog/CanDialog.java
@@ -47,13 +47,13 @@ import com.wnafee.vector.compat.Tintable;
 
 /**
  * Copyright 2016 canyinghao
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -1319,8 +1319,6 @@ public final class CanDialog extends FrameLayout {
     /**
      * 设置icon的类型
      *
-     * @param type
-     * @param color
      */
     public void setIconType(byte type, int color) {
 
